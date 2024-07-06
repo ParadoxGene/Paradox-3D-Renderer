8b7419d84c35dac4e9768f65ba943b1f525744ab
DeviantCoder <109912349+ParadoxGene@users.noreply.github.com>
2024-07-06 17:03:10 -0400

    Update renderer.c

--- api/c/paradox-3d-renderer/src/paradox-3d-renderer/renderer.c
+++ api/c/paradox-3d-renderer/src/paradox-3d-renderer/renderer.c
@@ -8,11 +8,23 @@ PARADOX_3D_RENDERER_API paradox_float32_t paradox_3d_renderer_clear_color[4];
 PARADOX_3D_RENDERER_API void paradox_3d_renderer_init()
 {
     for(int i = 0; i < 4; i++) paradox_3d_renderer_clear_color[i] = 0;
+    switch(paradox_gl_api_mode())
+    {
+    case PARADOX_OPENGL_API:
+        paradox_opengl_load_functions();
+        break;
+    default: break;
+    }
 }
 PARADOX_3D_RENDERER_API void paradox_3d_renderer_clear_screen()
 {
-    glClearColor(paradox_3d_renderer_clear_color[0], paradox_3d_renderer_clear_color[1], paradox_3d_renderer_clear_color[2], paradox_3d_renderer_clear_color[3]);
-    glClear(GL_COLOR_BUFFER_BIT);
+    switch(paradox_gl_api_mode())
+    {
+    case PARADOX_OPENGL_API:
+        glClearColor(paradox_3d_renderer_clear_color[0], paradox_3d_renderer_clear_color[1], paradox_3d_renderer_clear_color[2], paradox_3d_renderer_clear_color[3]);
+        glClear(GL_COLOR_BUFFER_BIT);
+        break;
+    }
 }
 PARADOX_3D_RENDERER_API void paradox_set_3d_renderer_clear_color(paradox_float32_t color[])
 {
