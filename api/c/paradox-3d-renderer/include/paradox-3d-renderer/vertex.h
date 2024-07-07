19eceb290a74b7b3b51cfccab8bba2798a0e6d6e
DeviantCoder <109912349+ParadoxGene@users.noreply.github.com>
2024-07-07 14:22:22 -0400

    updated vertex

--- api/c/paradox-3d-renderer/include/paradox-3d-renderer/vertex.h
+++ api/c/paradox-3d-renderer/include/paradox-3d-renderer/vertex.h
@@ -8,4 +8,8 @@ typedef struct paradox_vertex {
     paradox_float32_t tex_coords[2];
 } paradox_vertex;
 
+PARADOX_3D_RENDERER_API void paradox_set_vertex_position(paradox_vertex* vertex, paradox_float32_t position[]);
+PARADOX_3D_RENDERER_API void paradox_set_vertex_normal(paradox_vertex* vertex, paradox_float32_t normal[]);
+PARADOX_3D_RENDERER_API void paradox_set_vertex_tex_coords(paradox_vertex* vertex, paradox_float32_t tex_coords[]);
+
 #endif
--- api/c/paradox-3d-renderer/src/paradox-3d-renderer/vertex.c
+++ api/c/paradox-3d-renderer/src/paradox-3d-renderer/vertex.c
@@ -1,2 +1,19 @@
 #include <paradox-3d-renderer/vertex.h>
 
+PARADOX_3D_RENDERER_API void paradox_set_vertex_position(paradox_vertex* vertex, paradox_float32_t position[])
+{
+    vertex->position[0] = position[0];
+    vertex->position[1] = position[1];
+    vertex->position[2] = position[2];
+}
+PARADOX_3D_RENDERER_API void paradox_set_vertex_normal(paradox_vertex* vertex, paradox_float32_t normal[])
+{
+    vertex->normal[0] = normal[0];
+    vertex->normal[1] = normal[1];
+    vertex->normal[2] = normal[2];
+}
+PARADOX_3D_RENDERER_API void paradox_set_vertex_tex_coords(paradox_vertex* vertex, paradox_float32_t tex_coords[])
+{
+    vertex->tex_coords[0] = tex_coords[0];
+    vertex->tex_coords[1] = tex_coords[1];
+}
